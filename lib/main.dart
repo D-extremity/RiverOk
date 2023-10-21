dc4870ad2e571959bb7c663839a7cfdff6271285
Satyam Shrivastav <mysatyam99@gmail.com>
2023-10-22 00:50:56 +0530

    comments updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,25 @@ final userProvider = StateNotifierProvider<UserNotifier2, User>(
 //! always call function outside to change any parameter and also it is better way to keep logic and UI part separately 
 //! ref.read() is preferred to be called outside because it reads the value once (not continuous)
 void setText(WidgetRef ref, String value) {
-  ref.read(userProvider.notifier).updateName(value);
+  ref.read(userProvider.notifier).updateName(value);//!if .notifier would not be there , it would have been impossible to call .update method
 }
+//^.notifier is used to access the notifier object associated with a state provider, and it allows you to call methods and modify the state using the provided notifier.
+//^-->The state object: This represents the current value of the state.
+//^-->The notifier object: This is used to modify and update the state.
+/* In the code you provided, the .notifier property is used to access the notifier object associated with a Riverpod state provider. Here's an explanation of what it does:
+
+State Provider:
+It appears that userProvider is a Riverpod state provider. State providers in Riverpod typically consist of two parts:
+
+-->The state object: This represents the current value of the state.
+-->The notifier object: This is used to modify and update the state.
+The .notifier property allows you to access the notifier object associated with the state provider.
+
+ref.read(userProvider.notifier): This reads the notifier associated with the userProvider. By doing this, you can access and call methods on the notifier.
+
+.updateName(value): This is calling the updateName method on the notifier. It appears that updateName is a method defined in the notifier object, and it is being used to update the name in the state associated with the userProvider.
+
+So, in summary, .notifier is used to access the notifier object associated with a state provider, and it allows you to call methods and modify the state using the provided notifier. This is a common pattern in Riverpod for managing state.*/
 
 void main() {
   runApp(const MyApp());
